1b2abba2e33f833e90def8058c2413ec5383d121
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-04-12 17:16:31 -0600

    Made things more user friendly

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
-from tools.getTextFromReddit import redditScraper
-from tools.mapLocater import geoLocater
-from tools.plot import plot
-import pandas
-import sys
+#Check to make sure credential.py exists first
+try: from credential import reddit_client_id,reddit_client_secret,user_agent
+except: 
+    print("Please create a credential.py file, exiting gracefully")
+    import sys
+    sys.exit()
+
 import argparse
+parser = argparse.ArgumentParser(description="Gathers mentions of locations and cities from a Reddit post")
+parser.add_argument('-t',help="Extracts and looks up one location, for verification that everything is working",action='store_true')
+parser.add_argument('-p',help="Plots the results on a worldmap, to be displayed through your default browser",action='store_true')
+parser.add_argument('-code',metavar="-c",type=str,help="Reddit URL or post Code (typically 6 character code found in shortlink). Defaults to fzcrci.",default=None)
+parser.add_argument('-outputLocation',metavar="-o",type=str,help="Output file path and name of the parsed datafile, default csv if no extension is provided. Supports CSV,xlsx,json. Defaults to results.csv",default=None)
+parser.add_argument('-v',help="Verbose",action='store_true')
+args = parser.parse_args()
+
+if args.t:
+    if args.code == None: args.code = "fzcrci"
+    if args.outputLocation == None: args.outputLocation = "test.csv"
+elif args.code == None or args.outputLocation == None:
+    print("Not in testing mode, please provide values for code and outputLocation, -h for help")
+    import sys
+    sys.exit()
+
+import pandas
 from tqdm import tqdm
 
+from tools.getTextFromReddit import redditScraper
+from tools.mapLocater import geoLocater
+
 class redditLocation:
-    def __init__(self,url,method='geopy'):
+    def __init__(self):
         #initialize
-        self.url = url
         self.reddit = redditScraper()
-        self.geoLocater = geoLocater(method)
+        self.geoLocater = geoLocater(verbose=args.v)
     def comments(self):
-        self.reddit.getSubmission(url)
+        self.reddit.getSubmission(args.code)
         self.reddit.getComments()
     def getLocation(self,testConnections=False):
         resultArray=[]
         dataFrameColumns = ["display_name","type","lon","lat"]
-        for comment in tqdm(self.reddit.comments[0] if testConnections else self.reddit.comments,desc="Looking for addresses"):
+        comments = [self.reddit.comments[0]] if args.t else self.reddit.comments
+        for comment in tqdm(comments,desc="Looking for addresses"):
             #Iterate through possible search entries
             resultArray = resultArray + self.geoLocater.getFromComment(comment)
         
@@ -27,25 +49,27 @@ class redditLocation:
             print("Unable to build a dataframe of locations, this is most likely due to an error with connecting to Nominatim")
             self.locations=pandas.DataFrame(columns=dataFrameColumns)
         return self.locations
+    def export(self,fileName):
+        print("Exporting Results")
+        if fileName.endswith(".xlsx"): self.locations.to_excel(fileName)
+        elif fileName.endswith(".json"): self.locations.to_json(fileName)
+        elif fileName.endswith(".csv"): self.locations.to_csv(fileName)
+        else: self.locations.to_csv(self.filename + ".csv")
     def plotLocations(self):
+        from tools.plot import plot
         if self.locations.empty: print("Cannot Plot, Dataframe is empty")
         else: plot(self.locations)
-    def export(self,fileName):
-        self.locations.to_csv(fileName)
 
 if __name__ == "__main__":
-    testConnections = False
-    url = sys.argv[1]
-    outputLocation = sys.argv[2]
-    print("Parsing reddit thread of: {} \n And outputting data to: {}".format(url,outputLocation))
+    print("Parsing reddit thread of: {} \nAnd outputting data to: {}".format(args.code,args.outputLocation))
 
     print("Grabbing Thread From Reddit")
-    method = 'geopy'
-    redditLocation = redditLocation(url)
+    redditLocation = redditLocation()
     redditLocation.comments()
 
-    redditLocation.getLocation(testConnections=testConnections)
+    redditLocation.getLocation()
 
-    redditLocation.export(outputLocation)
+    redditLocation.export(args.outputLocation)
 
-    redditLocation.plotLocations()
+    if args.p:
+        redditLocation.plotLocations()
--- tools/mapLocater.py
+++ tools/mapLocater.py
@@ -2,18 +2,19 @@ from geopy.geocoders import Nominatim as geocoder
 from tools.SearchEntryGenerator import SearchEntryGenerator
 
 class geoLocater:
-    def __init__(self,method='geopy',tries=3):
-        if method=='geopy':
-            self.geolocator = geocoder(user_agent="redditMapGenerator")
-            self.tries = tries
+    def __init__(self,verbose=False):
+        self.geolocator = geocoder(user_agent="redditMapGenerator")
+        self.verbose = verbose
     def get(self,searchQuery):
-        for i in range(self.tries):
-            try: return self.geolocator.geocode(searchQuery,language="en",timeout=3)
+        for i in range(3): #try three times to connect to Nominatim, its sometimes unreliable
+            try:
+                return self.geolocator.geocode(searchQuery,language="en",timeout=3) #wait three seconds before timeout, this is because Nominatim is slow sometimes
             except: print("Could not get location for {}, this is most likely a timeout issue.".format(searchQuery))
         return None
     def getFromComment(self,comment):
         results=[]
         for searchEntry in SearchEntryGenerator(comment,want='all'):
+            if self.verbose: print("\nLooking for {} from =>: {}".format(searchEntry,comment.replace("\n"," ")))
             result = self.get(searchEntry)
             if result:
                 results.append(result.raw)
